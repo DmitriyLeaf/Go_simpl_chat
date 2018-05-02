75f3ae4d1a17724069df2ef4efb46b80575d8b06
Dmitry Kruhlov <dmitriy.kruglov.work@gmail.com>
2018-05-02 21:58:13 +0300

    Server tcp

---
+++ server.go
@@ -0,0 +1,57 @@
+package server
+
+import (
+	"fmt"
+	"net"
+	"bufio"
+	"container/list"
+	)
+
+var clients *list.List
+
+func Client(client net.Conn, ){
+	bufReader := bufio.NewReader(client)
+
+	for {
+		rline, err := bufReader.ReadString('\n')
+		
+		if err != nil {
+			fmt.Println("Cen not read!", err)
+			client.Close()
+			break
+		}
+
+		for i := clients.Front(); i != nil; i = i.Next() {
+			fmt.Println(client.RemoteAddr(), ": ", string(rline))
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Run server:")
+
+	clients = list.New()
+
+	server, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		fmt.Println("Error: %s", err.Error())
+		return
+	}
+
+	for {
+		client, err := server.Accept()
+
+		if err != nil {
+			fmt.Println("Error: %s", err.Error())
+			client.Close()
+			continue
+		}
+
+		clients.PushBack(client)
+		fmt.Println("Connected: ", client.RemoteAddr(), )
+		
+		go Client(client)
+	}
+
+}
